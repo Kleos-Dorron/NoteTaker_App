ae847c6d2eaff2ca02b3185efb2d1426d322cc6e
Kleos Dorron <kleos.kd@gmail.com>
2024-07-15 18:08:27 +0530

    Add project files.

---
+++ ViewModels/NotesViewModel.cs
@@ -0,0 +1,32 @@
+﻿using NoteTaker_App.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NoteTaker_App.ViewModels
+{
+    public class NotesViewModel : INotifyPropertyChanged
+    {
+        public ObservableCollection<Note> Notes { get; set; }
+
+        public NotesViewModel()
+        {
+            Notes = new ObservableCollection<Note>
+            {
+                new Note { Title = "Sample Note", Content = "This is a sample note.", DateCreated = DateTime.Now }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
